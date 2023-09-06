03d2304749947c355e3332872358d608970c32c4
Jack Meng <jackmeng0814@gmail.com>
2023-09-06 16:34:31 -0400

    `add`

---
+++ Introduction_To_C++/classwork_1.cc
@@ -0,0 +1,11 @@
+/*
+For today in class, create a program in C++ that prints to the screen on multiple lines using both "\n" and endl. Mix and match. Make sure you are comfortable creating a C++ file on the computer using either a local or internet-based IDE.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hel\n" << "lo\n" << "Wo" << endl << "rld";
+}
