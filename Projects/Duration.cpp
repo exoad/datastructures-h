e7aa309f41778f14d5fc35d02f36b86e8339d19f
exoad <jackmeng0814@gmail.com>
2023-11-26 17:35:20 -0500

    `add`

--- Projects/Duration.cpp
+++ Projects/Duration.cpp
@@ -1,5 +1,13 @@
+/**
+ * @file Duration.cpp
+ * @author Jack Meng
+ * @brief Demonstrates adding durations and time together using operator overloading
+ * @date 2023-11-26
+ */
+
 #include <iostream>
 #include <cstring>
+#include <cassert>
 
 using namespace std;
 
@@ -24,64 +32,112 @@ using namespace std;
                   		        if (___USERE != "Y" || ___USERE != "y") break;\
                         }
 
+/**
+ * @brief Makes sure the number is not negative [0, inf)
+ *
+ * @param i int
+ * @return int i if i>=0 else 0
+ */
 inline int clampZero(int i)
 {
   return i<0?0:i;
 }
 
+/**
+ * @brief Uses two places for numbers less than 10
+ *
+ * @param i int
+ * @return string "0"+i if i<10 else i
+ */
 inline string formatTwoSpaces(int i)
 {
   return i<10?"0"+to_string(i):to_string(i);
 }
 
+/**
+ * @brief A duration class that stores time in hours, minutes and seconds
+ */
 class Duration
 {
   private:
+  // private members
     int minutes = 0;
     int hours = 0;
     int seconds = 0;
 
     int totalSeconds;
 
+  // private methods for computing total seconds and syncing time with the other attributes
     void computeTotalSeconds(int h, int m, int s)
     {
       totalSeconds=h*3600+m*60+s;
     }
-
+  // syncing time with the other private members
     void syncTime()
     {
       hours=totalSeconds/3600;
-      totalSeconds/=3600;
-      minutes=totalSeconds/60;
-      totalSeconds/=60;
-      seconds=totalSeconds;
+      minutes=(totalSeconds%3600)/60;
+      seconds=totalSeconds%60;
     }
 
-  public:
-    Duration(int m=0, int h=0, int s=0)
+  // private constructor for setting by total seconds (for internal use)
+    Duration(int totalSeconds)
     {
-      computeTotalSeconds(h, m, s);
+      this->totalSeconds=totalSeconds;
       syncTime();
     }
 
+
+  public:
+    /**
+     * @brief Construct a new Duration object
+     *
+     * @param h hours
+     * @param m minutes
+     * @param s seconds
+     */
+    Duration(int h, int m, int s)
+    {
+      computeTotalSeconds(
+        clampZero(h),
+        clampZero(m),
+        clampZero(s)
+      );
+      syncTime();
+    }
+
+    /**
+     * @brief Adds two durations
+     *
+     * @param other Duration
+     * @return Duration sum of the two durations
+     */
     Duration operator+(Duration const& other)
     {
       return Duration(
-        this->minutes+other.minutes,
-        this->hours+other.hours,
-        this->seconds+other.seconds
+        this->totalSeconds+other.totalSeconds
       );
     }
 
+    /**
+     * @brief Subtracts two durations
+     *
+     * @param other Duration
+     * @return Duration difference of the two durations
+     */
     Duration operator-(Duration const& other)
     {
       return Duration(
-        this->minutes-other.minutes,
-        this->hours-other.hours,
-        this->seconds-other.seconds
+        clampZero(this->totalSeconds-other.totalSeconds)
       );
     }
 
+    /**
+     * @brief Multiplies a duration by a constant
+     *
+     * @param other int
+     * @return Duration product of the duration and the constant
+     */
     Duration operator*(int const& other)
     {
       return Duration(
@@ -91,8 +147,15 @@ class Duration
       );
     }
 
+    /**
+     * @brief Divides a duration by a constant
+     *
+     * @param other int
+     * @return Duration quotient of the duration and the constant
+     */
     Duration operator/(int const& other)
     {
+      assert(other!=0); // please dont do this :(
       return Duration(
         this->minutes/other,
         this->hours/other,
@@ -100,15 +163,27 @@ class Duration
       );
     }
 
+    /**
+     * @brief Divides a duration by another duration
+     *
+     * @param other Duration
+     * @return Duration quotient of the two durations
+     */
     Duration operator/(Duration const& other)
     {
+      assert(other.totalSeconds!=0); // please dont do this :(
       return Duration(
-        this->minutes/other.minutes,
-        this->hours/other.hours,
-        this->seconds/other.seconds
+        this->totalSeconds/other.totalSeconds
       );
     }
 
+    /**
+     * @brief For printing the duration in the format HH:MM:SS using the << operator (for cout)
+     *
+     * @param os ostream&
+     * @param d Duration
+     * @return ostream& os
+     */
     friend ostream& operator<<(ostream& os, Duration const& d)
     {
       string format;
@@ -125,12 +200,16 @@ class Duration
       return os;
     }
 
+    /**
+     * @brief Randomize the duration's time components
+     */
     void randomize()
     {
-      // not going to use the facilities in random.h even though they have better randomness :P
-      this->minutes=rand()%60;
-      this->hours=rand()%24;
-      this->seconds=rand()%60;
+      *this=Duration(
+        rand()%24,
+        rand()%60,
+        rand()%60
+      );
     }
 };
 
@@ -144,7 +223,8 @@ inline Duration askDurationObj()
 
 int main(void)
 {
+  // ref: https://planetcalc.com/65/
   USER_LOOP({
-    cout << Duration(30, 60, 30);
+    cout << (Duration(30, 60, 30)+Duration(30,30,30)); // 31:01:30+30:30:30=61:31:60=62:33:00
   })
 }
