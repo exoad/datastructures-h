2f45534f5c060e443868210525ebcaa80fcec14f
exoad <45053009+exoad@users.noreply.github.com>
2023-12-11 14:32:20 -0500

    Update ListLab1.h
    Signed-off-by: exoad <45053009+exoad@users.noreply.github.com>

--- Projects/ListLab1.h
+++ Projects/ListLab1.h
@@ -20,36 +20,55 @@ class ListNode
 
 string first(ListNode* head)
 {
-    
+    return head==NULL?NULL:head->getValue();
 }
 
 string second(ListNode* head)
 {
-    
+    return head==NULL||head->getNext()==NULL?NULL:head->getNext()->getValue();
 }
 
 ListNode* copyOfLast(ListNode* head)
 {
-    
+    ListNode* temp=pointerToLast(head);
+    return new ListNode(temp->getValue(),temp->getNext());
 }
 
 ListNode* pointerToLast(ListNode* head)
 {
-    
+    ListNode* curr=head;
+    while(curr!=NULL)
+        curr=curr->getNext();
+    return curr;
 }
 
 ListNode* insertFirst(ListNode* head, string arg)
 {
-    
+    return new ListNode(arg,head);
 }
 
 ListNode* insertLast(ListNode* head, string arg)
 {
-    
+    return new ListNode(arg,pointerToLast(head));
 }
 
 ListNode::ListNode(string value,ListNode* next) : value(value), next(next)
 {
+}
+
+ListNode* ListNode::copyNode(ListNode* arg) noexcept
+{
+    return new ListNode(arg->getValue(),arg->getNext());
+}
+
+ListNode* ListNode::copyList(ListNode* arg) noexcept
+{
+    
+}
+
+ListNode* ListNode::rest(ListNode* h) noexcept
+{
+    
 }
 
 ListNode* ListNode::getNext() 
@@ -61,4 +80,3 @@ string ListNode::getValue()
 {
     return value;
 }
-
