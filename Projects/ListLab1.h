abdbb8b9c7aad434a9fb01e694d0a8e5ab70bebd
exoad <45053009+exoad@users.noreply.github.com>
2023-12-11 14:17:46 -0500

    Create ListLab1.h
    Signed-off-by: exoad <45053009+exoad@users.noreply.github.com>

---
+++ Projects/ListLab1.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstring>
+
+using namespace std;
+
+class ListNode 
+{
+    private:
+        string value;
+        ListNode* next;  
+    public:
+        ListNode(string value,ListNode* next);
+        ListNode* copyNode(ListNode* arg);
+        ListNode* copyList(ListNode* arg);
+        ListNode* rest(ListNode* h);
+        string first(ListNode* head);
+        string second(ListNode* head);
+        ListNode* copyOfLast(ListNode* head);
+        ListNode* pointerToLast(ListNode* head);
+        ListNode* insertFirst(ListNode* head, string arg);
+        ListNode* insertLast(ListNode* head, string arg);
+        string getValue();
+        ListNode* getNext();
+};
+
+ListNode::ListNode(string value,ListNode* next) : value(value), next(next)
+{
+}
+
+ListNode* ListNode::getNext() 
+{
+    return next;
+}
+
+string ListNode::getValue()
+{
+    return value;
+}
+
